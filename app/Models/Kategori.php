79e0f859a876ff1529a9897dd050daf2cc1f9a3d
Fauzan Evan F <.>
2026-03-25 23:02:19 +0700

    feat: Menambahkan model Kategori dan memperbarui KategoriController untuk menggunakannya

--- app/Models/Kategori.php
+++ app/Models/Kategori.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Kategori extends Model
 {
-    //
+    protected $table = 'kategori'; // nama tabel di database
+
+    protected $primaryKey = 'id_kategori'; // karena kamu pakai id_kategori
+
+    protected $fillable = [
+        'nama',
+        'gambar'
+    ];
 }
